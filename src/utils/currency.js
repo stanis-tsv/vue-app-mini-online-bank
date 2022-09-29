6d40a6ded5475b1760191bcbc945a6acdc21a482
stanis-tsv <97790288+stanis-tsv@users.noreply.github.com>
2022-09-29 21:00:07 +0300

    Update currency.js

--- src/utils/currency.js
+++ src/utils/currency.js
@@ -1,4 +1,4 @@
-const formatter = new Intl.NumberFormat('ua-UA', {currency: 'UAH', style: 'currency'})
+const formatter = new Intl.NumberFormat('uk-UA', {currency: 'UAH', style: 'currency', currencyDisplay: 'narrowSymbol'})
 
 export function currency(value) {
   return formatter.format(value)
